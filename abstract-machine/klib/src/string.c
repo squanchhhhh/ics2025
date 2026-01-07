f4c47a5f39c750fe5ff316a2531d20c7861feb01
tracer-ics2025 <tracer@njuics.org>
2026-01-07 18:13:59 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-76-generic #83-Ubuntu SMP Thu Jun 15 19:21:56 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  18:13:59 up 9 days, 17:55,  1 user,  load average: 0.08, 0.17, 0.16

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -45,16 +45,13 @@ char *strcat(char *dst, const char *src) {
 }
 
 int strcmp(const char *s1, const char *s2) {
-  int index = 0;
-  while(s1[index] != '\0' && s2[index] != '\0'){
-    int temp = s1[index] - s2[index];
-    if(temp !=0){
-      return temp;
-    }else{
-      index++;
+    while(*s1 && *s2) {
+        int diff = (unsigned char)*s1 - (unsigned char)*s2;
+        if(diff != 0) return diff;
+        s1++;
+        s2++;
     }
-  }
-  return s1[index] - s2[index];
+    return (unsigned char)*s1 - (unsigned char)*s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
