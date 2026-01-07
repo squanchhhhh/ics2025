3c7659277b9700202ef1e647c1870d573804c76b
tracer-ics2025 <tracer@njuics.org>
2026-01-07 15:33:02 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-76-generic #83-Ubuntu SMP Thu Jun 15 19:21:56 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  15:33:02 up 9 days, 15:15,  1 user,  load average: 0.09, 0.11, 0.14

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -23,7 +23,7 @@
 #define Mw vaddr_write
 
 enum {
-  TYPE_I, TYPE_U, TYPE_S, TYPE_J, TYPE_R,
+  TYPE_I, TYPE_U, TYPE_S, TYPE_J, TYPE_R,TYPE_B,
   TYPE_N, // none
 };
 
@@ -32,13 +32,25 @@ enum {
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
-#define immJ() do { *imm =  (BITS(i, 31, 31) << 20) | \
-                            (BITS(i, 19, 12) << 12) | \
-                            (BITS(i, 20, 20) << 11) | \
-                            (BITS(i, 30, 21) << 1);  \
-                            *imm = SEXT(*imm, 21); \
-                            } while(0);
-#define immR() do {} while(0);
+#define immJ() do { \
+  *imm = ( \
+    (BITS(i, 31, 31) << 20) | \
+    (BITS(i, 19, 12) << 12) | \
+    (BITS(i, 20, 20) << 11) | \
+    (BITS(i, 30, 21) << 1)  \
+  ); \
+  *imm = SEXT(*imm, 21); \
+} while (0)
+#define immR() do {} while(0)
+#define immB() do { \
+  *imm = ( \
+    (BITS(i, 31, 31) << 12) | \
+    (BITS(i, 7, 7)   << 11) | \
+    (BITS(i, 30, 25) << 5)  | \
+    (BITS(i, 11, 8)  << 1)  \
+  ); \
+  *imm = SEXT(*imm, 13); \
+} while (0)
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst;
   int rs1 = BITS(i, 19, 15);
@@ -51,6 +63,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_N: break;
     case TYPE_J: immJ(); break;
     case TYPE_R: src1R(); src2R(); immR(); break;
+    case TYPE_B: src1R(); src2R(); break;
     default: panic("unsupported type = %d", type);
   }
 }
@@ -76,6 +89,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("????????????  ????? 000 ????? 00100 11", addi   , I, R(rd) = src1 + imm);
   INSTPAT("??????? ????? ????? 101 ????? 00100 11", slri   , I, R(rd) = ((sword_t)src1) >> imm);
   INSTPAT("????????????  ????? 011 ????? 00100 11", slriu  , I, R(rd) = src1>>imm);
+  INSTPAT("??????? ????? ????? 000 ????? 11000 11", beq    , B, if (src1 == src2) {s->dnpc = s->pc + imm;});
   INSTPAT("???????????????????? ?????    11011 11", jal    , J, R(rd) = s->pc + 4; s->dnpc = s->pc + imm;);
   INSTPAT("????????????  ????? 000 ????? 11001 11", jalr   , I, word_t ret = s->pc + 4;s->dnpc = (src1 + imm) & ~1;R(rd) = ret; );  
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
