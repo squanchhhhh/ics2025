f4e8f3018d48dd682ddf3c773ff97f19c955c795
tracer-ics2025 <tracer@njuics.org>
2026-01-08 19:12:20 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-76-generic #83-Ubuntu SMP Thu Jun 15 19:21:56 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  19:12:20 up 10 days, 15:39,  1 user,  load average: 0.80, 0.48, 0.29

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,73 +1,72 @@
 #include <am.h>
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 #include <stdarg.h>
 #include <string.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int itoa(char*buf,int x) {
-    char tmp[16];
-    int i = 0;
-    int neg = 0;
-    if (x == 0) {
-        buf[0] = '0';
-        return 1;
-    }
-    if (x < 0) {
-        neg = 1;
-        x = -x;
-    }
-    while (x > 0) {
-        tmp[i++] = '0' + (x % 10);
-        x /= 10;
-    }
-    int len = 0;
-    if (neg) buf[len++] = '-';
-    while (i > 0) {
-        buf[len++] = tmp[--i];
-    }
-    return len;
+int itoa(char *buf, int x) {
+  char tmp[16];
+  int i = 0;
+  int neg = 0;
+  if (x == 0) {
+    buf[0] = '0';
+    return 1;
+  }
+  if (x < 0) {
+    neg = 1;
+    x = -x;
+  }
+  while (x > 0) {
+    tmp[i++] = '0' + (x % 10);
+    x /= 10;
+  }
+  int len = 0;
+  if (neg)
+    buf[len++] = '-';
+  while (i > 0) {
+    buf[len++] = tmp[--i];
+  }
+  return len;
 }
 
-int printf(const char *fmt, ...) {
-  panic("Not implemented");
-}
+int printf(const char *fmt, ...) { panic("Not implemented"); }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-    va_list ap;
-    va_start(ap, fmt);
-    int out_index = 0;
-    while (*fmt) {
-        if (*fmt != '%') {
-            out[out_index++] = *fmt++;
-            continue;
-        }
-        fmt++; 
-        switch (*fmt) {
-        case 'd': {
-            int x = va_arg(ap, int);
-            int len = itoa(&out[out_index],x);
-            out_index += len;
-            break;
-        }
-        case 's': {
-            char *s = va_arg(ap, char *);
-            int len = strlen(s);
-            memcpy(&out[out_index], s, len);
-            out_index += len;
-            break;
-        }
-      }
-        fmt++;
+  va_list ap;
+  va_start(ap, fmt);
+  int out_index = 0;
+  while (*fmt) {
+    if (*fmt != '%') {
+      out[out_index++] = *fmt++;
+      continue;
     }
-    out[out_index] = '\0';
-    va_end(ap);
-    return out_index;
+    fmt++;
+    switch (*fmt) {
+    case 'd': {
+      int x = va_arg(ap, int);
+      int len = itoa(&out[out_index], x);
+      out_index += len;
+      break;
+    }
+    case 's': {
+      char *s = va_arg(ap, char *);
+      int len = strlen(s);
+      memcpy(&out[out_index], s, len);
+      out_index += len;
+      break;
+    }
+    }
+    fmt++;
+  }
+  out[out_index] = '\0';
+  va_end(ap);
+  return out_index;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
