2d27bd38208f7a49ac03bc11846db90cc2b70f16
tracer-ics2025 <tracer@njuics.org>
2026-01-09 16:00:09 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-76-generic #83-Ubuntu SMP Thu Jun 15 19:21:56 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  16:00:09 up 11 days, 10:58,  1 user,  load average: 0.11, 0.15, 0.18

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -166,7 +166,7 @@ void cpu_exec(uint64_t n) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
     case NEMU_ABORT:  Log("Recent instructions:");
                       recent_insts(&iringbuf);
-    case NEMU_END:
+    case NEMU_END: 
       Log("nemu: %s at pc = " FMT_WORD,
           (nemu_state.state == NEMU_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) :
            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
