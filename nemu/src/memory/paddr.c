b0cf31bbbc301ac0fce2c98234265633cebbf30a
tracer-ics2025 <tracer@njuics.org>
2026-01-09 16:57:22 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-76-generic #83-Ubuntu SMP Thu Jun 15 19:21:56 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  16:57:22 up 11 days, 11:56,  1 user,  load average: 0.35, 0.28, 0.21

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -53,6 +53,9 @@ void init_mem() {
 word_t paddr_read(paddr_t addr, int len) {
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
+  #ifdef CONFIG_MTRACE
+    
+  #endif
   out_of_bound(addr);
   return 0;
 }
