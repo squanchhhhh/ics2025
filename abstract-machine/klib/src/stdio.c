f19d8bae2b4a17f559f95a298c9d3e1258a9d18c
tracer-ics2025 <tracer@njuics.org>
2026-01-12 12:08:07 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-76-generic #83-Ubuntu SMP Thu Jun 15 19:21:56 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  12:08:07 up 14 days,  1:33,  1 user,  load average: 0.12, 0.17, 0.11

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -2,44 +2,12 @@
 #include <klib-macros.h>
 #include <klib.h>
 #include <stdarg.h>
+#include <stdio.h>
 #include <string.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-
-int itoa(char *buf, int x) {
-  char tmp[16];
-  int i = 0;
-  int neg = 0;
-  if (x == 0) {
-    buf[0] = '0';
-    return 1;
-  }
-  if (x < 0) {
-    neg = 1;
-    x = -x;
-  }
-  while (x > 0) {
-    tmp[i++] = '0' + (x % 10);
-    x /= 10;
-  }
-  int len = 0;
-  if (neg)
-    buf[len++] = '-';
-  while (i > 0) {
-    buf[len++] = tmp[--i];
-  }
-  return len;
-}
-
-int printf(const char *fmt, ...) { panic("Not implemented"); }
-
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
-}
-
-int sprintf(char *out, const char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
+int itoa(char *buf, int x);
+int fmt_resolve(char * out,const char * fmt,va_list ap){
   int out_index = 0;
   while (*fmt) {
     if (*fmt != '%') {
@@ -65,8 +33,55 @@ int sprintf(char *out, const char *fmt, ...) {
     fmt++;
   }
   out[out_index] = '\0';
-  va_end(ap);
   return out_index;
+}
+int itoa(char *buf, int x) {
+  char tmp[16];
+  int i = 0;
+  int neg = 0;
+  if (x == 0) {
+    buf[0] = '0';
+    return 1;
+  }
+  if (x < 0) {
+    neg = 1;
+    x = -x;
+  }
+  while (x > 0) {
+    tmp[i++] = '0' + (x % 10);
+    x /= 10;
+  }
+  int len = 0;
+  if (neg)
+    buf[len++] = '-';
+  while (i > 0) {
+    buf[len++] = tmp[--i];
+  }
+  return len;
+}
+
+int printf(const char *fmt, ...) {
+  char buf[256];
+  va_list ap;
+  va_start(ap, fmt);
+  int len = fmt_resolve(buf, fmt, ap);
+  va_end(ap);
+  for (int i = 0;i<len;i++){
+    putch(buf[i]);
+  }
+  return len;
+}
+
+int vsprintf(char *out, const char *fmt, va_list ap) {
+  panic("Not implemented");
+}
+
+int sprintf(char *out, const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  int len = fmt_resolve(out,fmt,ap);
+  va_end(ap);
+  return len;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
