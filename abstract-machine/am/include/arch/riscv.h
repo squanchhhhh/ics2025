47048f8bca22836908b8a072854a668fd16a4856
tracer-ics2025 <tracer@njuics.org>
2026-01-17 01:19:10 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-76-generic #83-Ubuntu SMP Thu Jun 15 19:21:56 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  01:19:10 up 17 days,  6:19,  1 user,  load average: 0.17, 0.14, 0.10

--- abstract-machine/am/include/arch/riscv.h
+++ abstract-machine/am/include/arch/riscv.h
@@ -6,11 +6,12 @@
 #else
 #define NR_REGS 32
 #endif
-
 struct Context {
-  // TODO: fix the order of these members to match trap.S
-  uintptr_t mepc, mcause, gpr[NR_REGS], mstatus;
-  void *pdir;
+  uintptr_t gpr[NR_REGS]; // 偏移 0 到 124 
+  uintptr_t mcause;       // 偏移 128
+  uintptr_t mstatus;      // 偏移 132
+  uintptr_t mepc;         // 偏移 136
+  void *pdir;            
 };
 
 #ifdef __riscv_e
@@ -19,9 +20,8 @@ struct Context {
 #define GPR1 gpr[17] // a7
 #endif
 
-#define GPR2 gpr[0]
-#define GPR3 gpr[0]
-#define GPR4 gpr[0]
-#define GPRx gpr[0]
-
+#define GPR2 gpr[10] // a0: 第 1 个参数
+#define GPR3 gpr[11] // a1: 第 2 个参数
+#define GPR4 gpr[12] // a2: 第 3 个参数
+#define GPRx gpr[10] // a0: 返回值寄存器
 #endif
