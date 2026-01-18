345d30d50e684e04d3190afb6017f601891d37b9
tracer-ics2025 <tracer@njuics.org>
2026-01-18 21:26:45 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-76-generic #83-Ubuntu SMP Thu Jun 15 19:21:56 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  21:26:45 up 18 days,  3:38,  1 user,  load average: 0.31, 0.31, 0.24

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,10 +1,24 @@
 #include <common.h>
 #include "syscall.h"
-void do_syscall(Context *c) {
+void do_syscall(Context *ctx) {
   uintptr_t a[4];
-  a[0] = c->GPR1;
+  a[0] = ctx->GPR1; 
+  a[1] = ctx->GPR2; 
+  a[2] = ctx->GPR3; 
+  a[3] = ctx->GPR4;
 
   switch (a[0]) {
-    default: panic("Unhandled syscall ID = %d", a[0]);
+    case SYS_yield:
+      yield();
+      ctx->GPRx = 0; 
+      break;
+
+    case SYS_exit:
+      Log("Process exited with code %d", a[1]);
+      halt(0); 
+      break;
+
+    default: 
+      panic("Unhandled syscall ID = %d", a[0]);
   }
 }
