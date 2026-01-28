a0884fe546c60f25681ae58b221a59c237146276
tracer-ics2025 <tracer@njuics.org>
2026-01-28 11:54:24 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-76-generic #83-Ubuntu SMP Thu Jun 15 19:21:56 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  11:54:24 up 21 days, 21:15,  7 users,  load average: 0.37, 0.28, 0.19

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -146,7 +146,7 @@ static int decode_exec(Decode *s) {
   #ifdef CONFIG_FTRACE
   if(rd == 1){
     int fid = find_func_by_addr(s->pc);
-        if (fid >= 0) ftrace_record(s->pc, fid, TRACE_CALL);
+        if (fid >= 0) ftrace_record(target, fid, TRACE_CALL);
   }
   if (rd == 0 && BITS(s->isa.inst, 19, 15) == 1){
     Log("ret rd:%d,src1:%x",rd,src1);   // src1是取值后的值！！！
