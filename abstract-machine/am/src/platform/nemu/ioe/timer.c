2c93edc81bbfcef8f3b7f002ca0932575eda8334
tracer-ics2025 <tracer@njuics.org>
2026-01-29 14:42:42 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-76-generic #83-Ubuntu SMP Thu Jun 15 19:21:56 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  14:42:42 up 22 days, 11:31,  7 users,  load average: 0.28, 0.19, 0.15

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -18,8 +18,8 @@ do {
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
   rtc->second = 0;
   rtc->minute = 0;
-  rtc->hour   = 0;
-  rtc->day    = 0;
-  rtc->month  = 0;
-  rtc->year   = 1900;
+  rtc->hour   = *(volatile uint32_t *)(RTC_ADDR + 8);
+  rtc->day    = *(volatile uint32_t *)(RTC_ADDR + 12);
+  rtc->month  = *(volatile uint32_t *)(RTC_ADDR + 16);
+  rtc->year   = *(volatile uint32_t *)(RTC_ADDR + 24);
 }
