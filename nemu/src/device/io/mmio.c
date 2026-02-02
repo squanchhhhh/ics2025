47e91d754d98403465d46d21128cf32268df87c1
tracer-ics2025 <tracer@njuics.org>
2026-02-02 20:02:30 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-76-generic #83-Ubuntu SMP Thu Jun 15 19:21:56 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  20:02:30 up 24 days, 19:02,  7 users,  load average: 0.12, 0.11, 0.11

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -59,5 +59,8 @@ word_t mmio_read(paddr_t addr, int len) {
 }
 
 void mmio_write(paddr_t addr, int len, word_t data) {
+  if (addr >= 0xa0000300 && addr <= 0xa000030c) {
+    printf("MMIO WRITE DETECTED: addr=0x%08x, data=0x%08x\n", addr, data);
+  }
   map_write(addr, len, data, fetch_mmio_map(addr));
 }
