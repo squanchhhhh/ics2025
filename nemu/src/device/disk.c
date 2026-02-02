b9d6e378a18cfa1a46e771cda6963ed893710146
tracer-ics2025 <tracer@njuics.org>
2026-02-02 20:12:47 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-76-generic #83-Ubuntu SMP Thu Jun 15 19:21:56 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  20:12:47 up 24 days, 19:12,  7 users,  load average: 0.45, 0.28, 0.19

--- nemu/src/device/disk.c
+++ nemu/src/device/disk.c
@@ -50,25 +50,21 @@ static void handle_disk_transfer() {
   }
 }
 static uint32_t *disk_base = NULL; // 模仿串口，改用指针
+
 static void disk_io_handler(uint32_t offset, int len, bool is_write) {
-  // 模仿串口：不管 len 到底是多少，先看是不是写了 CTRL 寄存器
+    printf("call disk_io_handler and offset = %d,len=%d,iswrite=%d\n",offset,len,is_write);
   if (is_write && offset == DISK_CTRL_REG) {
-    // 此时数据已经由 mmio_write 填入 disk_base[2] 了（如果是先填数据后调用的逻辑）
-    // 或者我们需要手动同步
     handle_disk_transfer();
   }
 }
 void init_disk() {
-  // 1. 获取镜像路径
   const char *path = getenv("DISK_IMG");
   if (path == NULL) return;
   disk_fp = fopen(path, "r+b");
   Assert(disk_fp, "Cannot open disk image");
 
-  // 2. 模仿串口：申请空间
-  disk_base = (uint32_t *)new_space(12); // 3个寄存器 * 4字节
+  disk_base = (uint32_t *)new_space(12); 
 
-  // 3. 注册：注意最后一个参数是回调函数
   add_mmio_map("disk", CONFIG_DISK_CTL_MMIO, disk_base, 12, disk_io_handler);
 }
 
