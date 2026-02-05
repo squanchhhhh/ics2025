f5c29641930218ab423830fa21eda68c83e25a3b
tracer-ics2025 <tracer@njuics.org>
2026-02-05 21:20:23 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-76-generic #83-Ubuntu SMP Thu Jun 15 19:21:56 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  21:20:23 up 26 days, 12:57,  7 users,  load average: 0.27, 0.27, 0.23

--- nanos-lite/src/fscore.c
+++ nanos-lite/src/fscore.c
@@ -241,9 +241,8 @@ uint32_t namei(const char *path) {
     if (iget(inum, &curr_inode) < 0) return 0;
     while ((path = skipelem(path, name)) != NULL) {
         printf("path %s\n",path);
-
         uint32_t next_inum = dir_lookup(&curr_inode, name);
-                printf("%d\n",next_inum);
+        printf("next_inum %d\n",next_inum);
         if (next_inum == 0) return 0; 
         inum = next_inum;
         if (iget(inum, &curr_inode) < 0) return 0;
