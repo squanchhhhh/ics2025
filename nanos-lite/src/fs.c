d7fdf9cef243f21c46e2b79c13a0609ae4fd53ea
tracer-ics2025 <tracer@njuics.org>
2026-02-06 20:29:39 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-76-generic #83-Ubuntu SMP Thu Jun 15 19:21:56 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  20:29:39 up 27 days,  1:16,  7 users,  load average: 0.39, 0.22, 0.19

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -114,7 +114,7 @@ int alloc_system_fd(int f_idx, int flags) {
       } else {
         system_open_table[i].open_offset = 0;
       }
-      printf("alloc sys_open_table idx %d, point to fdx %d\n",i,f_idx);
+      printf("alloc sys_open_table idx %d, point to fdx %d, name in fdx %s\n",i,f_idx,file_table[f_idx].name);
       file_table[f_idx].ref++; 
       return i;
     }
