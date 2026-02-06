aa148208e64c0af0090b619d5bb83f5a2d40afb7
tracer-ics2025 <tracer@njuics.org>
2026-02-06 20:11:52 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-76-generic #83-Ubuntu SMP Thu Jun 15 19:21:56 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  20:11:52 up 27 days, 58 min,  7 users,  load average: 0.12, 0.18, 0.17

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -151,8 +151,8 @@ int sys_open(const char *path, int flags, mode_t mode) {
     int s_idx = vfs_open(path, flags);
     if (s_idx < 0) return -1;
     int fd = map_to_proc_fd(s_idx);
-    Log("[Syscall] Process '%s' mapped System Index %d to local FD %d", 
-            current->name, s_idx, fd);
+    Log("[Syscall] Process '%s' mapped System Index %d (%s) to local FD %d", 
+            current->name, s_idx,file_table[system_open_table[s_idx].file_idx].name, fd);
     return fd; 
 }
 
