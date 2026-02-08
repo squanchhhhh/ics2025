166fe91b1bff86f29ea515325227c891658fab5c
tracer-ics2025 <tracer@njuics.org>
2026-02-08 23:24:59 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-76-generic #83-Ubuntu SMP Thu Jun 15 19:21:56 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  23:24:59 up 28 days,  1:58,  7 users,  load average: 0.06, 0.15, 0.16

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,7 +18,21 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  for (int i = 0; i < 32; i++) {
+    if (ref_r->gpr[i] != cpu.gpr[i]) {
+      printf("Check failed at gpr[%d]: expected 0x%08x, but got 0x%08x\n", 
+              i, ref_r->gpr[i], cpu.gpr[i]);
+      return false;
+    }
+  }
+
+  if (ref_r->pc != pc) {
+    printf("Check failed at PC: expected 0x%08x, but got 0x%08x\n", 
+            ref_r->pc, pc);
+    return false;
+  }
+
+  return true;
 }
 
 void isa_difftest_attach() {
