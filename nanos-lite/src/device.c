6e1df4b5f22a5dc08f28b935b4cba51a80457bb1
tracer-ics2025 <tracer@njuics.org>
2026-02-09 12:48:55 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-76-generic #83-Ubuntu SMP Thu Jun 15 19:21:56 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  12:48:55 up 28 days,  8:31,  7 users,  load average: 0.93, 0.45, 0.27

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -28,7 +28,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 
     return 0;
   }
-  Log("Key event detected: keycode=%d, down=%d", ev.keycode, ev.keydown);
+  //Log("Key event detected: keycode=%d, down=%d", ev.keycode, ev.keydown);
   int ret = snprintf(buf, len, "%s %s\n", 
                      ev.keydown ? "kd" : "ku", 
                      keyname[ev.keycode]);
