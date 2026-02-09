92ec207caee249340fa0141d018032ec03e43457
tracer-ics2025 <tracer@njuics.org>
2026-02-10 04:18:54 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-76-generic #83-Ubuntu SMP Thu Jun 15 19:21:56 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  04:18:54 up 28 days, 22:34,  7 users,  load average: 0.25, 0.19, 0.19

--- abstract-machine/am/src/native/ioe/gpu.c
+++ abstract-machine/am/src/native/ioe/gpu.c
@@ -50,6 +50,9 @@ void __am_gpu_status(AM_GPU_STATUS_T *stat) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  if (ctl->pixels == NULL) {
+    return;
+  }
   int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
   if (w == 0 || h == 0) return;
   feclearexcept(-1);
