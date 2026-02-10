e42abd3b463a37199b53f960bd8b81b4fadfe055
tracer-ics2025 <tracer@njuics.org>
2026-02-10 19:57:52 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-76-generic #83-Ubuntu SMP Thu Jun 15 19:21:56 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  19:57:52 up 29 days, 12:17,  7 users,  load average: 0.15, 0.16, 0.17

--- nemu/src/monitor/trace/itrace.c
+++ nemu/src/monitor/trace/itrace.c
@@ -11,31 +11,70 @@ void push_inst(vaddr_t pc, const char *s) {
   if (iringbuf.num < RING_SIZE) iringbuf.num++;
 }
 
+// 传入文件名和行号，返回这一行的代码内容
+char* get_source_line_content(const char* filename, int line) {
+    static char line_buf[256];
+    FILE *fp = fopen(filename, "r");
+    if (!fp) return "Source not found";
+
+    int cur = 0;
+    while (fgets(line_buf, sizeof(line_buf), fp)) {
+        cur++;
+        if (cur == line) {
+            fclose(fp);
+            line_buf[strcspn(line_buf, "\r\n")] = 0;
+            return line_buf;
+        }
+    }
+    fclose(fp);
+    return "Line not found";
+}
+typedef struct {
+    vaddr_t pc;
+    char filename[256];
+    int line;
+} SourceInfo;
+
 void print_recent_insts() {
-  printf("------- [ Recent Instructions (itrace) ] -------\n");
-  int cnt = iringbuf.num;
-  
-  char filename[256];
-  int line_num;
+    int cnt = iringbuf.num;
+    SourceInfo src_list[RING_SIZE];
 
-  for (int i = 0; i < cnt; i++) {
-    get_pc_source(iringbuf.pc[i], filename, &line_num);
-    if (i == iringbuf.error_idx) {
-      printf("--> ");
-    } else {
-      printf("    ");
+    printf("------- [ Recent Instructions (itrace) ] -------\n");
+
+    printf("asm:\n");
+    for (int i = 0; i < cnt; i++) {
+        src_list[i].pc = iringbuf.pc[i];
+        get_pc_source(src_list[i].pc, src_list[i].filename, &src_list[i].line);
+
+        const char *prefix = (i == iringbuf.error_idx) ? "-->" : "   ";
+        printf("%s %-50s (PC: 0x%08x)\n", prefix, iringbuf.buf[i], iringbuf.pc[i]);
     }
 
-    if (line_num != -1) {
-      char *short_filename = strrchr(filename, '/');
-      short_filename = (short_filename) ? short_filename + 1 : filename;
+    printf("\nsrc:\n");
 
-      printf("%-50s | %s:%d\n", iringbuf.buf[i], short_filename, line_num);
-    } else {
-      printf("%-50s | [unknown]\n", iringbuf.buf[i]);
+    for (int i = 0; i < cnt - 1; i++) {
+        for (int j = 0; j < cnt - i - 1; j++) {
+            if (src_list[j].pc > src_list[j + 1].pc) {
+                SourceInfo temp = src_list[j];
+                src_list[j] = src_list[j + 1];
+                src_list[j + 1] = temp;
+            }
+        }
     }
-  }
-  printf("------------------------------------------------\n");
+    for (int i = 0; i < cnt; i++) {
+        if (src_list[i].line != -1) {
+            char *content = get_source_line_content(src_list[i].filename, src_list[i].line);
+            
+            char *short_name = strrchr(src_list[i].filename, '/');
+            short_name = (short_name) ? short_name + 1 : src_list[i].filename;
+
+            printf("    [0x%08x] %s:%d | %s\n", 
+                   src_list[i].pc, short_name, src_list[i].line, content);
+        } else {
+            printf("    [0x%08x] unknown source\n", src_list[i].pc);
+        }
+    }
+    printf("------------------------------------------------\n");
 }
 void init_iring(){
   iringbuf.tail = 0;
