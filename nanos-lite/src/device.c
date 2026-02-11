9f951a748a4c8853b61d5c9272c80c0e29c92131
tracer-ics2025 <tracer@njuics.org>
2026-02-11 19:08:00 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-168-generic #178-Ubuntu SMP Fri Jan 9 19:29:48 UTC 2026 aarch64 aarch64 aarch64 GNU/Linux  19:08:00 up 18:57,  1 user,  load average: 0.20, 0.19, 0.16

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -16,7 +16,8 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  printf("call serial write\n");
+  yield();
+  //printf("call serial write\n");
   const char *str = (const char *)buf;
   for (size_t i = 0; i < len; i++) {
     putch(str[i]); 
