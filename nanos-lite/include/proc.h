6d3693924ff0a588b3eb362144ff062b52fe5ec1
tracer-ics2025 <tracer@njuics.org>
2026-02-11 19:58:26 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-168-generic #178-Ubuntu SMP Fri Jan 9 19:29:48 UTC 2026 aarch64 aarch64 aarch64 GNU/Linux  19:58:26 up 19:46,  1 user,  load average: 0.21, 0.27, 0.22

--- nanos-lite/include/proc.h
+++ nanos-lite/include/proc.h
@@ -14,7 +14,7 @@ typedef union {
     // we do not free memory, so use `max_brk' to determine when to call _map()
     uintptr_t max_brk;
     int fd_table[MAX_NR_PROC_FILE];                 
-    char *name;
+    char name[64];
   };
 } PCB;
 
