0a4cfd145655c16fd3134d4c6bab8d31e2569d40
tracer-ics2025 <tracer@njuics.org>
2026-02-12 05:25:45 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-168-generic #178-Ubuntu SMP Fri Jan 9 19:29:48 UTC 2026 aarch64 aarch64 aarch64 GNU/Linux  05:25:45 up 1 day,  4:58,  1 user,  load average: 0.09, 0.18, 0.20

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -16,7 +16,33 @@
 #include <isa.h>
 #include <memory/vaddr.h>
 #include <memory/paddr.h>
-
+/*
+功能：将虚拟地址翻译成物理地址
+1. 从 satp 寄存器获取一级页目录的物理基地址 (PPN)
+2. 提取虚拟地址的索引
+3. 一级页表查询 (Page Directory)
+4. 二级页表查询 (Page Table)
+5. 拼接最终的物理地址
+*/
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
-  return MEM_RET_FAIL;
+  paddr_t pg_dir_base = (cpu.csr.satp & 0x3fffff) << 12;
+
+  uint32_t vpn1 = (vaddr >> 22) & 0x3ff;
+  uint32_t vpn0 = (vaddr >> 12) & 0x3ff;
+  uint32_t offset = vaddr & 0xfff;
+
+  paddr_t pde_addr = pg_dir_base + vpn1 * 4;
+  uint32_t pde = paddr_read(pde_addr, 4);
+
+  assert(pde & 0x1); 
+
+  paddr_t pg_tab_base = (pde >> 10) << 12;
+  paddr_t pte_addr = pg_tab_base + vpn0 * 4;
+  uint32_t pte = paddr_read(pte_addr, 4);
+
+  assert(pte & 0x1);
+
+  paddr_t pa = ((pte >> 10) << 12) | offset;
+
+  return pa;
 }
