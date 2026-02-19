74b8c3aaf8c478795c7082a88743d0ffcc1b6dcf
tracer-ics2025 <tracer@njuics.org>
2026-02-19 18:44:04 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-168-generic #178-Ubuntu SMP Fri Jan 9 19:29:48 UTC 2026 aarch64 aarch64 aarch64 GNU/Linux  18:44:04 up 2 days,  5:55,  3 users,  load average: 0.08, 0.16, 0.17

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -26,21 +26,34 @@
 */
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   word_t satp = cpu.csr.satp;
-  if (!(satp & 0x80000000)) return MEM_RET_OK; 
+  
+  // 如果 satp 没开启 (MODE != 1)，直接返回物理地址
+  if (!(satp & 0x80000000)) return vaddr; 
 
   paddr_t pg_dir_base = (satp & 0x3fffff) << 12;
 
+  // 一级页表
   uint32_t vpn1 = (vaddr >> 22) & 0x3ff;
   paddr_t pte1_addr = pg_dir_base + vpn1 * 4;
   word_t pte1 = paddr_read(pte1_addr, 4);
 
+  if (!(pte1 & 0x1)) {
+    // 这里的格式直接写死，避开 FMT_VADDR 宏
+    Log("MMU L1 Error: vaddr=0x%08x, pte1_addr=0x%08x, pte1=0x%08x, satp=0x%08x", 
+        vaddr, pte1_addr, pte1, satp);
+  }
   assert(pte1 & 0x1); 
 
+  // 二级页表
   paddr_t pg_tab_base = (pte1 >> 10) << 12;
   uint32_t vpn0 = (vaddr >> 12) & 0x3ff;
   paddr_t pte0_addr = pg_tab_base + vpn0 * 4;
   word_t pte0 = paddr_read(pte0_addr, 4);
 
+  if (!(pte0 & 0x1)) {
+    Log("MMU L0 Error: vaddr=0x%08x, pte0_addr=0x%08x, pte0=0x%08x", 
+        vaddr, pte0_addr, pte0);
+  }
   assert(pte0 & 0x1);
 
   paddr_t paddr = ((pte0 >> 10) << 12) | (vaddr & 0xfff);
