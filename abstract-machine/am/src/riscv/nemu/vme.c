951f42edc91032d8cf492dd3535df09a208485d6
tracer-ics2025 <tracer@njuics.org>
2026-02-19 18:17:52 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-168-generic #178-Ubuntu SMP Fri Jan 9 19:29:48 UTC 2026 aarch64 aarch64 aarch64 GNU/Linux  18:17:52 up 2 days,  5:29,  3 users,  load average: 0.38, 0.23, 0.17

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -63,7 +63,10 @@ void __am_get_cur_as(Context *c) {
 
 void __am_switch(Context *c) {
   if (vme_enable && c->pdir != NULL) {
+    printf("Switching to pdir: %p", c->pdir);
     set_satp(c->pdir);
+  } else {
+    printf("Stay in kernel pdir");
   }
 }
 
