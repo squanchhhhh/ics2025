3cfdbae72836fc9a175197c93195789694356bb5
tracer-ics2025 <tracer@njuics.org>
2026-02-19 15:04:51 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-168-generic #178-Ubuntu SMP Fri Jan 9 19:29:48 UTC 2026 aarch64 aarch64 aarch64 GNU/Linux  15:04:51 up 2 days,  2:16,  3 users,  load average: 0.20, 0.12, 0.10

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -83,10 +83,9 @@ static void exec_once(Decode *s, vaddr_t pc) {
   void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
   disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst, ilen);
-#endif
-  #ifdef CONFIG_ITRACE
   push_inst(s->pc, s->logbuf, &s->reg_res);
-  #endif
+#endif
+
 }
 
 static void execute(uint64_t n) {
