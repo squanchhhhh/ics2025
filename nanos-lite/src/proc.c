1b187e4220d0dbe757f2a7be47cdec86458cf47a
tracer-ics2025 <tracer@njuics.org>
2026-02-20 11:25:15 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-168-generic #178-Ubuntu SMP Fri Jan 9 19:29:48 UTC 2026 aarch64 aarch64 aarch64 GNU/Linux  11:25:15 up 2 days, 21:48,  3 users,  load average: 0.02, 0.10, 0.10

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -31,7 +31,7 @@ void init_proc() {
   //char *envp[] = {"PATH=/bin:/usr/bin", NULL};
   char *argv[] = {NULL};
   char *envp[] = {NULL};
-  context_uload(&pcb[1], "/bin/dummy", argv, envp);
+  context_uload(&pcb[1], "/bin/hello", argv, envp);
 
   switch_boot_pcb();
 }
