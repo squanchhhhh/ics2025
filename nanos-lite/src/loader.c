345870dbd82ff73f985e79fc930a5aac9e5135c4
tracer-ics2025 <tracer@njuics.org>
2026-02-20 11:35:36 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-168-generic #178-Ubuntu SMP Fri Jan 9 19:29:48 UTC 2026 aarch64 aarch64 aarch64 GNU/Linux  11:35:36 up 2 days, 21:59,  3 users,  load average: 0.45, 0.29, 0.17

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -104,6 +104,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   // 5. 创建上下文
   Area kstack = RANGE(pcb->stack, pcb->stack + sizeof(pcb->stack));
   pcb->cp = ucontext(&pcb->as, kstack, (void *)entry);
+  printf("user proc pdir = %x\n",v_ptr);
   pcb->cp->gpr[10] = argc;
   pcb->cp->gpr[2] = v_ptr;
 }
