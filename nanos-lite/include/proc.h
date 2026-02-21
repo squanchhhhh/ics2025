d4a14e9e67d9a84560ae06abfad1874aefc28d6a
tracer-ics2025 <tracer@njuics.org>
2026-02-21 19:11:52 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-168-generic #178-Ubuntu SMP Fri Jan 9 19:29:48 UTC 2026 aarch64 aarch64 aarch64 GNU/Linux  19:11:52 up 4 days,  3:07,  3 users,  load average: 0.07, 0.15, 0.17

--- nanos-lite/include/proc.h
+++ nanos-lite/include/proc.h
@@ -6,7 +6,7 @@
 void do_execve(const char *filename, char *const argv[], char *const envp[]) ;
 #define STACK_SIZE (8 * PGSIZE)
 #define MAX_NR_PROC_FILE 32
-/*
+
 typedef union {
   uint8_t stack[STACK_SIZE] PG_ALIGN;
   struct {
@@ -18,17 +18,7 @@ typedef union {
     char name[64];
   };
 } PCB;
- */
- typedef struct {
-  // 管理信息放在最前面
-  Context *cp;
-  AddrSpace as;
-  uintptr_t max_brk;
-  int fd_table[MAX_NR_PROC_FILE];                 
-  char name[64];
-  // 栈放在最后面，且不需要放在 union 里
-  uint8_t stack[STACK_SIZE] PG_ALIGN;
-} PCB;
+
 
 extern PCB *current;
 void naive_uload(PCB *pcb, const char *filename);
