01be43ed22c57f1b7c3939eed0624b1e761f9da5
tracer-ics2025 <tracer@njuics.org>
2026-02-21 12:44:12 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-168-generic #178-Ubuntu SMP Fri Jan 9 19:29:48 UTC 2026 aarch64 aarch64 aarch64 GNU/Linux  12:44:12 up 3 days, 21:01,  3 users,  load average: 0.70, 0.25, 0.18

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -25,7 +25,7 @@ void do_syscall(Context *ctx) {
   a[1] = ctx->GPR2; 
   a[2] = ctx->GPR3; 
   a[3] = ctx->GPR4;
-
+  printf("call syscall a[0]=%d, a[1]=%d, a[2]=%d, a[3]=%d\n",a[0],a[1],a[2],a[3]);
   switch (a[0]) {
     case SYS_yield:
       ctx->GPRx = 0; 
