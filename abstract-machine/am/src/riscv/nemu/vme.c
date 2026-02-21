fa95a753d49a2ab2e9b4cafd3955152216881b48
tracer-ics2025 <tracer@njuics.org>
2026-02-21 14:47:15 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-168-generic #178-Ubuntu SMP Fri Jan 9 19:29:48 UTC 2026 aarch64 aarch64 aarch64 GNU/Linux  14:47:15 up 3 days, 23:04,  3 users,  load average: 0.11, 0.12, 0.14

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -65,21 +65,11 @@ void __am_get_cur_as(Context *c) {
 
 void __am_switch(Context *c) {
   if (c == NULL) return;
+  // 永远从 Context 拿到原始物理地址或使用内核地址
+  void *pdir = (c->pdir != NULL) ? c->pdir : kas.ptr;
   
-  // 1. 获取顶级页表物理地址
-  void *pdir_addr = (c->pdir != NULL) ? c->pdir : kas.ptr;
-
-  // 2. 调用原有的 set_satp (确保当前内核环境能正常访存)
-  set_satp(pdir_addr);
-
-  // 3. 【核心修正】将 pdir 字段修改为硬件 satp 格式
-  // 这样接下来的 trap.S 在恢复上下文时，lw t0 加载到的就是 0x8008043c
-  uintptr_t mode = 1ul << 31;
-  uintptr_t ppn = (uintptr_t)pdir_addr >> 12;
-  c->pdir = (void *)(mode | ppn); 
-
-  // 4. 同步 TLB
-  asm volatile("sfence.vma"); 
+  // 转换并写入硬件
+  set_satp(pdir); 
 }
 /*
 功能：在页表中填入页表项
