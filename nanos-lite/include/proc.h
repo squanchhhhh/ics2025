faa82e4c737580bfcb5dd0ebe06d6f6db8b465c9
tracer-ics2025 <tracer@njuics.org>
2026-02-21 19:31:10 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-168-generic #178-Ubuntu SMP Fri Jan 9 19:29:48 UTC 2026 aarch64 aarch64 aarch64 GNU/Linux  19:31:10 up 4 days,  3:27,  3 users,  load average: 1.01, 0.52, 0.31

--- nanos-lite/include/proc.h
+++ nanos-lite/include/proc.h
@@ -7,16 +7,13 @@ void do_execve(const char *filename, char *const argv[], char *const envp[]) ;
 #define STACK_SIZE (8 * PGSIZE)
 #define MAX_NR_PROC_FILE 32
 
-typedef union {
+typedef struct {\
+  Context *cp;
+  AddrSpace as;
+  uintptr_t max_brk;
+  int fd_table[MAX_NR_PROC_FILE];                 
+  char name[64];
   uint8_t stack[STACK_SIZE] PG_ALIGN;
-  struct {
-    Context *cp;
-    AddrSpace as;
-    // we do not free memory, so use `max_brk' to determine when to call _map()
-    uintptr_t max_brk;
-    int fd_table[MAX_NR_PROC_FILE];                 
-    char name[64];
-  };
 } PCB;
 
 
