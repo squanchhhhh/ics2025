f7c8d104cfb80337fb97de85ee760b3e23adc14a
tracer-ics2025 <tracer@njuics.org>
2026-02-22 21:14:24 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu-linux-22-04-02-desktop 5.15.0-168-generic #178-Ubuntu SMP Fri Jan 9 19:29:48 UTC 2026 aarch64 aarch64 aarch64 GNU/Linux  21:14:24 up 5 days,  3:43,  3 users,  load average: 0.13, 0.30, 0.28

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,20 +1,24 @@
-#include <common.h>
-#include "syscall.h"
 #include "proc.h"
+#include "syscall.h"
+#include <common.h>
 void do_syscall(Context *ctx);
-static Context* do_event(Event e, Context* c) {
+static Context *do_event(Event e, Context *c) {
   switch (e.event) {
-    case EVENT_YIELD:
-      return schedule(c);
-      //printf("Yield event recognized!\n");
-      break;
-    case EVENT_SYSCALL:
-      //printf("Syscall event recognized!\n");
-      do_syscall(c);
-      printf("[DEBUG] Syscall done. Old c: %p, New cp: %p\n", c, current->cp);
-      return current->cp;
-    default: 
-      panic("Unhandled event ID = %d", e.event);
+  case EVENT_YIELD:
+    return schedule(c);
+    // printf("Yield event recognized!\n");
+    break;
+  case EVENT_SYSCALL: {
+    uintptr_t old_cp = (uintptr_t)c;
+    do_syscall(c);
+    if ((uintptr_t)current->cp != old_cp) {
+      printf("[DEBUG] Context changed during syscall! Old: %p, New: %p\n",
+             old_cp, current->cp);
+    }
+    return current->cp;
+  }
+  default:
+    panic("Unhandled event ID = %d", e.event);
   }
   return c;
 }
